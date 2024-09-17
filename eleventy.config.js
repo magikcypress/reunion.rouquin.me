7b6a77e3c31f3012cded5da8ec301aa071948d6c
magikcypress <cyp@rouquin.me>
2024-09-17 18:13:09 +0400

    Update eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -120,45 +120,6 @@ module.exports = function(eleventyConfig) {
 		});
 	});
 
-	eleventyConfig.addNunjucksAsyncShortcode("Image", async (src, alt) => {
-		if (!alt) {
-		  throw new Error(`Missing \`alt\` on myImage from: ${src}`);
-		}
-	
-		let stats = await Image(src, {
-		  widths: [25, 320, 640, 960, 1200, 1800, 2400],
-		  formats: ["jpeg", "webp"],
-		  urlPath: "/images/",
-		  outputDir: "./_site/images/",
-		});
-	
-		let lowestSrc = stats["jpeg"][0];
-	
-		const srcset = Object.keys(stats).reduce(
-		  (acc, format) => ({
-			...acc,
-			[format]: stats[format].reduce(
-			  (_acc, curr) => `${_acc} ${curr.srcset} ,`,
-			  ""
-			),
-		  }),
-		  {}
-		);
-	
-		const source = `<source type="image/webp" srcset="${srcset["webp"]}" >`;
-	
-		const img = `<img
-		  loading="lazy"
-		  alt="${alt}"
-		  src="${lowestSrc.url}"
-		  sizes='(min-width: 1024px) 1024px, 100vw'
-		  srcset="${srcset["jpeg"]}"
-		  width="${lowestSrc.width}"
-		  height="${lowestSrc.height}">`;
-	
-		return `<div class="image-wrapper"><picture> ${source} ${img} </picture></div>`;
-	  });
-
 	// Features to make your build faster (when you need them)
 
 	// If your passthrough copy gets heavy and cumbersome, add this line
