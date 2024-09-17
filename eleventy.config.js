7f899ff6201951c9b7ec964d33cdb80d8d6821c4
magikcypress <cyp@rouquin.me>
2024-09-17 18:01:04 +0400

    update eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -9,6 +9,7 @@ const pluginBundle = require("@11ty/eleventy-plugin-bundle");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const { EleventyHtmlBasePlugin, EleventyRenderPlugin  } = require("@11ty/eleventy");
 const emojiReadTime = require("@11tyrocks/eleventy-plugin-emoji-readtime");
+const Image = require("@11ty/eleventy-img");
 
 let options = {
     // whatever options you have set for the library here
@@ -118,6 +119,45 @@ module.exports = function(eleventyConfig) {
 			slugify: eleventyConfig.getFilter("slugify")
 		});
 	});
+
+	eleventyConfig.addNunjucksAsyncShortcode("Image", async (src, alt) => {
+		if (!alt) {
+		  throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+		}
+	
+		let stats = await Image(src, {
+		  widths: [25, 320, 640, 960, 1200, 1800, 2400],
+		  formats: ["jpeg", "webp"],
+		  urlPath: "/images/",
+		  outputDir: "./_site/images/",
+		});
+	
+		let lowestSrc = stats["jpeg"][0];
+	
+		const srcset = Object.keys(stats).reduce(
+		  (acc, format) => ({
+			...acc,
+			[format]: stats[format].reduce(
+			  (_acc, curr) => `${_acc} ${curr.srcset} ,`,
+			  ""
+			),
+		  }),
+		  {}
+		);
+	
+		const source = `<source type="image/webp" srcset="${srcset["webp"]}" >`;
+	
+		const img = `<img
+		  loading="lazy"
+		  alt="${alt}"
+		  src="${lowestSrc.url}"
+		  sizes='(min-width: 1024px) 1024px, 100vw'
+		  srcset="${srcset["jpeg"]}"
+		  width="${lowestSrc.width}"
+		  height="${lowestSrc.height}">`;
+	
+		return `<div class="image-wrapper"><picture> ${source} ${img} </picture></div>`;
+	  });
 
 	// Features to make your build faster (when you need them)
 
